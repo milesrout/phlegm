03bc00173601b7e41a072759680ef565f01cdcb9
Miles Rout <miles.rout@gmail.com>
2017-12-03 20:53:44 +1300

    Add support for some more tactics

--- Formulae.hs
+++ Formulae.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PatternSynonyms #-}
 
 module Formulae where
 
+import Data.List
+import qualified Data.Set as Set
+import Data.Set (Set, (\\))
+import Debug.Trace
 import Text.Printf
 
 data Formula = FProp Proposition
@@ -11,7 +16,43 @@ data Formula = FProp Proposition
              | FImpl Formula Formula
              | FForall String Formula
              | FExists String Formula
-             deriving Eq
+             deriving (Show, Eq, Ord)
+
+--subst x a b = trace ("\nSUBST[" ++ show x ++ " FOR " ++ show a ++ " IN " ++ show b ++ "]") $ subst x a b
+
+class Substitutable a b where
+    subst :: String -> a -> b -> b
+
+class Render a where
+    render :: a -> String
+
+class FreeVariables a where
+    freeVariables :: a -> Set String
+    freeVars :: [a] -> Set String
+    freeVars as = Set.unions (map freeVariables as)
+
+instance Render a => Render [a] where
+    render as = intercalate "," (map render as)
+
+instance Substitutable Atom Formula where
+    subst x a (FProp p) = FProp (subst x a p)
+    subst x a (FConj l r) = FConj (subst x a l) (subst x a r)
+    subst x a (FImpl l r) = FImpl (subst x a l) (subst x a r)
+    subst x a (FDisj l r) = FDisj (subst x a l) (subst x a r)
+    subst x a f@(FForall y b)
+        | x == y    = f
+        | otherwise = FForall y (subst x a b)
+    subst x a f@(FExists y b)
+        | x == y    = f
+        | otherwise = FExists y (subst x a b)
+
+instance FreeVariables Formula where
+    freeVariables (FProp p) = freeVariables p
+    freeVariables (FConj l r) = freeVars [l, r]
+    freeVariables (FImpl l r) = freeVars [l, r]
+    freeVariables (FDisj l r) = freeVars [l, r]
+    freeVariables (FForall x a) = Set.insert x (freeVariables a)
+    freeVariables (FExists x a) = Set.insert x (freeVariables a)
 
 pattern x :∧ y = FConj x y
 pattern x :∨ y = FDisj x y
@@ -23,33 +64,54 @@ x → y = FImpl x y
 x ∀ y = FForall x y
 x ∃ y = FExists x y
 
-instance Show Formula where
-    show (FProp p) = show p
-    show (FConj a b) = printf "(%s ∧ %s)" (show a) (show b)
-    show (FDisj a b) = printf "(%s ∨ %s)" (show a) (show b)
-    show (FImpl a b) = printf "(%s → %s)" (show a) (show b)
-    show (FForall x f) = printf "∀%s.%s" x (show f)
-    show (FExists x f) = printf "∃%s.%s" x (show f)
+instance Render Formula where
+    render (FProp p) = render p
+    render (FConj a b) = printf "(%s ∧ %s)" (render a) (render b)
+    render (FDisj a b) = printf "(%s ∨ %s)" (render a) (render b)
+    render (FImpl a b) = printf "(%s → %s)" (render a) (render b)
+    render (FForall x f) = printf "∀%s.%s" x (render f)
+    render (FExists x f) = printf "∃%s.%s" x (render f)
 
-data PredSymbol = PredSymbol String Integer deriving Eq
-instance Show PredSymbol where
-    show (PredSymbol name _) = name
+data PredSymbol = PredSymbol String Integer deriving (Eq, Ord, Show)
+instance Render PredSymbol where
+    render (PredSymbol name _) = name
 
-data FuncSymbol = FuncSymbol String Integer deriving Eq
-instance Show FuncSymbol where
-    show (FuncSymbol name _) = name
+data FuncSymbol = FuncSymbol String Integer deriving (Eq, Ord, Show)
+instance Render FuncSymbol where
+    render (FuncSymbol name _) = name
 
-data Atom = AVar String | AApp FuncSymbol [Atom] deriving Eq
-instance Show Atom where
-    show (AVar name) = name
-    show (AApp f xs) = printf "%s(%s)" (show f) (show xs)
+data Atom = AVar String | AApp FuncSymbol [Atom] deriving (Eq, Ord, Show)
+instance Render Atom where
+    render (AVar name) = name
+    render (AApp f xs) = printf "%s(%s)" (render f) (render xs)
+instance Substitutable Atom Atom where
+    subst x a b@(AVar y)
+        | x == y    = a
+        | otherwise = b
+    subst x a b@(AApp f@(FuncSymbol y n) as)
+        | x == y    = b
+        | otherwise = (AApp f bs)
+            where bs = map (subst x a) as
+instance FreeVariables Atom where
+    freeVariables (AVar x) = Set.singleton x
+    freeVariables (AApp (FuncSymbol p _) as) = Set.insert p (freeVars as)
 
-data Proposition = PApp PredSymbol [Atom] deriving Eq
-instance Show Proposition where
-    show (PApp p []) = show p
-    show (PApp p xs) = printf "%s(%s)" (show p) (show xs)
+data Proposition = PApp PredSymbol [Atom] deriving (Eq, Ord, Show)
+instance Render Proposition where
+    render (PApp p []) = render p
+    render (PApp p xs) = printf "%s(%s)" (render p) (render xs)
+instance Substitutable Atom Proposition where
+    subst x a (PApp p as) = (PApp p bs)
+        where bs = map (subst x a) as
+instance FreeVariables Proposition where
+    freeVariables (PApp (PredSymbol p _) as) = Set.insert p (freeVars as)
 
 pattern Prop s = FProp (PApp (PredSymbol s 0) [])
 pattern A = Prop "A"
 pattern B = Prop "B"
 pattern C = Prop "C"
+
+pattern P x = FProp (PApp (PredSymbol "P" 1) [x])
+pattern Q x = FProp (PApp (PredSymbol "Q" 1) [x])
+
+x = AVar "x"
--- Phlegm.hs
+++ Phlegm.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
@@ -9,17 +11,33 @@ import Formulae
 
 import Control.Lens hiding (children)
 import Control.Monad.RWS
+import Data.Maybe
+import qualified Data.Set as Set
+import Data.Set (Set, (\\))
 import Data.Tree
 import Data.Tree.Lens
 import Debug.Trace
 import Text.Printf
+
+unimplemented = error "Not yet implemented"
+
+(%~?) :: ASetter' s a -> (a -> Maybe a) -> (s -> s)
+l %~? f = l %~ (\t -> case f t of
+                          Nothing -> t
+                          Just s -> s)
+
+(%=?) :: MonadState s m => ASetter' s a -> (a -> Maybe a) -> m ()
+l %=? f = modify (l %~? f)
 
 data PhlegmCommand a where
     PAssertNamed :: String -> Formula -> PhlegmCommand ()
     PAssert :: Formula -> PhlegmCommand ()
     PAssumption :: PhlegmCommand ()
     PIntro :: PhlegmCommand ()
+    PElimImpl :: Formula -> PhlegmCommand ()
+    PElimForall :: PhlegmCommand ()
     PSplit :: PhlegmCommand ()
+    PNext :: PhlegmCommand ()
     PQed :: PhlegmCommand ()
 
 type Phlegm = Freer PhlegmCommand 
@@ -27,7 +45,10 @@ assertNamed n f = eta (PAssertNamed n f)
 assert f = eta (PAssert f)
 assumption = eta PAssumption
 intro = eta PIntro
+elimForall = eta PElimForall
+elimImpl f = eta (PElimImpl f)
 split = eta PSplit
+next = eta PNext
 qed = eta PQed
 
 data Goal = Goal
@@ -37,18 +58,20 @@ data Goal = Goal
     , _proved     :: Bool
     } deriving Show
 
-data Branches = Branches
-    { _value  :: Goal
-    , _lefts  :: [Tree Goal]
-    , _rights :: [Tree Goal]
-    , _base   :: Maybe Branches
-    } deriving Show
+data Branches a = Branches
+    { _value  :: a
+    , _lefts  :: [Tree a]
+    , _rights :: [Tree a]
+    , _base   :: Maybe (Branches a)
+    } deriving (Functor, Show)
 
-data Goals = Goals
-    { _focus    :: Goal
-    , _children :: [Tree Goal]
-    , _parent   :: Maybe Branches
-    } deriving Show
+data Zipper a = Goals
+    { _focus    :: a
+    , _children :: [Tree a]
+    , _parent   :: Maybe (Branches a)
+    } deriving (Functor, Show)
+
+type Goals = Zipper Goal
 
 data ProverState = ProverState
     { _goals   :: Goals
@@ -57,41 +80,73 @@ data ProverState = ProverState
 
 makeLenses ''Goal
 makeLenses ''Branches
-makeLenses ''Goals
+makeLenses ''Zipper
 makeLenses ''ProverState
 
-unicodeTick = "Y"
-unicodeCross = "N"
+unicodeTick = "✓"
+unicodeCross = "✗"
 
-abbreviateGoal g = printf "%s %s ⊢ %s" p hs f
+abbreviateGoal g = printf "%s %s: %s ⊢ %s" p n hs f
     where p = if g^.proved then unicodeTick else unicodeCross
-          hs = show (g^.hypotheses)
-          f = show (g^.formula)
+          n = g^.name
+          hs = render (g^.hypotheses)
+          f = render (g^.formula)
 
-toTree :: Goals -> Tree Goal
+abbreviateGoals :: Goals -> Zipper String
+abbreviateGoals gs = (fmap abbreviateGoal gs) & focus %~ (++ " <--")
+
+toTree :: Zipper a -> Tree a
 toTree gs = case gs^.parent of
     Nothing -> Node (gs^.focus) (gs^.children)
     Just _  -> toTree (unsafeMoveToParent gs)
 
 drawGoals :: Goals -> String
-drawGoals = drawTree . (fmap abbreviateGoal) . toTree
+drawGoals = drawTree . toTree . abbreviateGoals
 
 instance Show ProverState where
     show gs = drawGoals (gs^.goals)
 
-moveToParent :: Goals -> Maybe Goals
+moveToRight :: Zipper a -> Maybe (Zipper a)
+moveToRight gs =
+    case gs^.parent of
+        Nothing -> Nothing
+        Just bs -> case bs^.rights of
+            []    -> Nothing
+            (_:_) -> Just (unsafeMoveToRight gs)
+
+disassembleFocus :: Tree a -> Branches a -> Zipper a
+disassembleFocus t p = Goals { _focus = t^.root
+                             , _children = t^.branches
+                             , _parent = Just p }
+
+unsafeMoveToRight :: Zipper a -> Zipper a
+unsafeMoveToRight gs = disassembleFocus r p
+    where (Just bs) = gs^.parent
+          (r:rs) = bs^.rights
+          l = reassembleFocus gs
+          p = bs & lefts %~ (++[l])
+                 & rights .~ rs
+          --p = Branches { _value = bs^.value
+          --             , _lefts = bs^.lefts ++ [l]
+          --             , _rights = rs
+          --             , _base = bs^.base }
+
+reassembleFocus :: Zipper a -> Tree a
+reassembleFocus gs = Node (gs^.focus) (gs^.children)
+
+moveToParent :: Zipper a -> Maybe (Zipper a)
 moveToParent gs =
     case gs^.parent of
         Nothing -> Nothing
         Just _ -> Just (unsafeMoveToParent gs)
 
-moveToChild :: Int -> Goals -> Maybe Goals
+moveToChild :: Int -> Zipper a -> Maybe (Zipper a)
 moveToChild n gs = case gs^?children.ix n of
     Nothing -> Nothing
     Just child -> Just (unsafeMoveToChild gs (ls, m, rs))
         where (ls, (m:rs)) = splitAt n (gs^.children)
 
-unsafeMoveToChild :: Goals -> ([Tree Goal], Tree Goal, [Tree Goal]) -> Goals
+unsafeMoveToChild :: Zipper a -> ([Tree a], Tree a, [Tree a]) -> Zipper a
 unsafeMoveToChild gs (ls,m,rs) = newGoals
     where newGoals = Goals { _focus = m^.root
                            , _children = m^.branches
@@ -101,14 +156,13 @@ unsafeMoveToChild gs (ls,m,rs) = newGoals
                         , _rights = rs
                         , _base = gs^.parent }
 
-unsafeMoveToParent :: Goals -> Goals
+unsafeMoveToParent :: Zipper a -> Zipper a
 unsafeMoveToParent gs = Goals { _focus = nf, _children = nc, _parent = np }
     where (Just bs) = gs^.parent
           nf = bs^.value
           nc = (bs^.lefts) ++ middle ++ (bs^.rights)
           np = bs^.base
-          middle = [Node g (gs^.children)]
-          g = gs^.focus
+          middle = [reassembleFocus gs]
 
 data ProverReader = ProverReader
     {
@@ -124,19 +178,33 @@ instance Monoid ProverWriter where
 
 type Prover a = RWS ProverReader ProverWriter ProverState a
 
-freshName :: Prover String
-freshName = do
+freshGoal, freshVar :: Prover String
+freshGoal = freshName 'G'
+freshVar = freshName 'x'
+
+freshName :: Char -> Prover String
+freshName k = do
     n <- counter <+= 1
-    return ('x':show n)
+    return (k:show n)
 
-runPhlegm :: Phlegm a -> Prover a
-runPhlegm (Pure x) = pure x
-runPhlegm (Free m q) = do
-    a <- unPhlegm m
-    runPhlegm (q a)
+checkGoals :: Prover Bool
+checkGoals = do
+    oldGoals <- use goals
+    when (all checkTree (oldGoals^.children)) (goals %= propagateProvedness)
+    newGoals <- use goals
+    return (allProved newGoals)
 
-checkGoals :: Goals -> Bool
-checkGoals = checkTree . toTree
+allProved :: Goals -> Bool
+allProved = checkTree . toTree
+
+propagateProvedness :: Goals -> Goals
+propagateProvedness gs = do
+    if all (^.root.proved) (gs^.children)
+        then let updated = gs & focus.proved .~ True in
+               case moveToParent updated of
+                   Nothing -> updated
+                   Just hs -> propagateProvedness hs
+        else gs
 
 checkTree :: Tree Goal -> Bool
 checkTree = all (^.proved)
@@ -149,7 +217,7 @@ namedNewGoal n f hs = Goal { _name = n
 
 newGoal :: Formula -> [Formula] -> Prover Goal
 newGoal f hs = do
-    name <- freshName
+    name <- freshGoal
     return (namedNewGoal name f hs)
 
 subGoal :: Goal -> Prover ()
@@ -158,47 +226,133 @@ subGoal g = do
     goals.children %= (node:)
     goChild 0
 
+go :: (Goals -> Maybe Goals) -> Prover ()
+go f = goAlt [f, moveToParent]
+
+goAlt :: [Goals -> Maybe Goals] -> Prover ()
+goAlt [] = return ()
+goAlt (f:fs) = do
+    old <- use goals
+    case f old of
+        Nothing  -> goAlt fs
+        Just new -> goals .= new
+
+goRight :: Prover ()
+goRight = go moveToRight
+
 goChild :: Int -> Prover ()
-goChild n = do
-    oldGoals <- use goals
-    let (Just newGoals) = moveToChild n oldGoals
-    goals .= newGoals
+goChild n = go (moveToChild n)
 
 goParent :: Prover ()
-goParent = do
-    oldGoals <- use goals
-    let (Just newGoals) = moveToParent oldGoals
-    goals .= newGoals
+goParent = go moveToParent
+
+runPhlegm :: Phlegm a -> Prover a
+runPhlegm (Pure x) = pure x
+runPhlegm (Free m q) = do
+    a <- traceUnPhlegm m
+    runPhlegm (q a)
+
+traceUnPhlegm :: PhlegmCommand a -> Prover a
+traceUnPhlegm cmd = do
+    gs <- use goals
+    trace (drawGoals gs) $ unPhlegm cmd
+
+moveToNextSubGoal :: Prover Bool
+moveToNextSubGoal = do
+    gs <- use goals
+    case moveToRight gs of
+        Just hs -> do goals .= hs
+                      loopMoveToFirstChild
+                      return True
+        Nothing -> case moveToParent gs of
+            Just hs -> do goals .= hs
+                          goals.focus.proved .= True
+                          moveToNextSubGoal
+                          return True
+            Nothing -> return False
+
+loopMoveToFirstChild :: Prover ()
+loopMoveToFirstChild = do
+    gs <- use goals
+    case moveToChild 0 gs of
+        Just hs -> do goals .= hs
+                      loopMoveToFirstChild
+        Nothing -> return ()
+
+nextSubGoal :: Prover ()
+nextSubGoal = do
+    moveToNextSubGoal
+    return ()
 
 unPhlegm :: PhlegmCommand a -> Prover a
-unPhlegm (PAssert f)        = (newGoal f []) >>= subGoal
 unPhlegm (PAssertNamed n f) = subGoal (namedNewGoal n f [])
-unPhlegm PIntro = do
-    (FImpl a b) <- use (goals.focus.formula)
+
+unPhlegm (PAssert g) = do
+    f <- use (goals.focus.formula)
     hs <- use (goals.focus.hypotheses)
-    g <- newGoal b (hs ++ [a])
-    subGoal g
-unPhlegm PSplit = do
-    (FConj a b) <- use (goals.focus.formula)
-    hs <- use (goals.focus.hypotheses)
-    gB <- newGoal b hs
-    subGoal gB
+    newGoal f (hs ++ [g]) >>= subGoal
     goParent
-    gA <- newGoal a hs
-    subGoal gA
+    newGoal g hs >>= subGoal
+
 unPhlegm PAssumption = do
     f <- use (goals.focus.formula)
     hs <- use (goals.focus.hypotheses)
     if elem f hs
-        then goals.focus.proved .= True
+        then do goals.focus.proved .= True
         else error "No it isn't!"
+    nextSubGoal
+
+unPhlegm PIntro = do
+    f <- use (goals.focus.formula)
+    case f of
+        FImpl a b -> do
+            hs <- use (goals.focus.hypotheses)
+            newGoal b (hs ++ [a]) >>= subGoal
+        FForall x a -> do
+            hs <- use (goals.focus.hypotheses)
+            name <- freshVar
+            g <- newGoal (subst x (AVar name) a) hs
+            subGoal g
+        _ -> unimplemented
+
+unPhlegm (PElimImpl x) = do
+    f <- use (goals.focus.formula)
+    hs <- use (goals.focus.hypotheses)
+    when (not (elem x hs)) (traceShow hs $ error "That just doesn't work, mate!")
+    let hs' = Set.elems (Set.delete x (Set.fromList hs))
+    let f' = FImpl x f
+    newGoal f' hs' >>= subGoal
+
+unPhlegm PElimForall = do
+    f <- use (goals.focus.formula)
+    hs <- use (goals.focus.hypotheses)
+    let foralls = filter (\x -> case x of
+                                    FForall _ _ -> True
+                                    _ -> False) hs
+    let substs = do
+        fv <- Set.elems (freeVariables f)
+        (FForall x a) <- foralls
+        return $ subst x (AVar fv) a
+    if elem f substs
+        then do goals.focus.proved .= True
+        else error "That doesn't seem to be possible!"
+    nextSubGoal
+
+unPhlegm PSplit = do
+    (FConj a b) <- use (goals.focus.formula)
+    hs <- use (goals.focus.hypotheses)
+    newGoal b hs >>= subGoal
     goParent
+    newGoal a hs >>= subGoal
+
+unPhlegm PNext = nextSubGoal
+
 unPhlegm PQed = do
+    done <- checkGoals
     gs <- use goals
-    trace (drawGoals gs) $ if checkGoals gs
+    if done
         then return()
         else error "Nope!"
-
 
 runProver :: Formula -> Phlegm a -> (a, ProverState, ProverWriter)
 runProver f m = runRWS (runPhlegm m) r s
@@ -208,7 +362,7 @@ runProver f m = runRWS (runPhlegm m) r s
           gs = Goals { _focus = g
                      , _children = []
                      , _parent = Nothing }
-          g = Goal { _name = "base"
+          g = Goal { _name = "G"
                    , _formula = f
                    , _hypotheses = []
                    , _proved = False }
